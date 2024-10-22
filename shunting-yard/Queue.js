f90be4ce70df3c133d35887cbb5264f5d4adf88c
buan0001 <bustermikkel@gmail.com>
2024-10-22 16:39:15 +0200

    Added queue and stack classes, finished shunting yard algo

---
+++ shunting-yard/Queue.js
@@ -0,0 +1,107 @@
+
+export default class Queue {
+  head = null;
+  tail = null;
+
+  constructor(head = null) {
+    this.head = head;
+    this.tail = head;
+  }
+
+  [Symbol.iterator]() {
+    let current = this.tail;
+
+    return {
+      next: () => {
+        if (current) {
+          const returnVal = current;
+          current = current.next;
+          return { value: returnVal, done: false };
+        } else {
+          return { done: true };
+        }
+      },
+    };
+  }
+
+  // Add new elements to tail
+  enqueue(data) {
+    const newNode = new Node(data);
+    if (this.tail) {
+      // Make every node point backwards
+      this.tail.next = newNode;
+    }
+    // Before setting the new node as tail
+    this.tail = newNode;
+    if (!this.head) {
+      this.head = newNode;
+    }
+  }
+
+  // Remove elements from head
+  dequeue() {
+    if (!this.head) {
+      return;
+    }
+    let temp = this.head.data;
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return temp
+  }
+
+  peek() {
+    return this.head;
+  }
+
+
+  get(index) {
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      if (!current) {
+        return;
+      }
+      current = current.next;
+    }
+    return current;
+  }
+
+  size() {
+    let current = this.head;
+    let count = 0;
+    while (current) {
+      count++;
+      current = current.next;
+    }
+    console.log("count", count);
+    return count;
+  }
+  dumpList() {
+    let current = this.head;
+    let printString = ""
+    while (current) {
+      // console.log(current.data);
+      printString += `${current.data} `
+      current = current.next;
+    }
+    console.log(printString);
+    
+  }
+}
+
+class Node {
+  data = null;
+  next = null;
+  constructor(data = null, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+
+  setNext(newNext) {
+    this.next = newNext;
+  }
+}
---
+++ shunting-yard/yard.js
@@ -0,0 +1,83 @@
+import Queue from "./Queue.js";
+import Stack from "./Stack.js";
+
+function convertInfixToPostfix(infixString) {
+  console.log("Infix string:", infixString);
+
+  const tempInput = infixString.trim().split(" ");
+
+  const inputQueue = new Queue();
+  const outputQueue = new Queue();
+  const operatorStack = new Stack();
+
+  tempInput.forEach((element) => {
+    inputQueue.enqueue(element);
+  });
+
+
+  const precedence = {
+    "(": 4,
+    ")": 4,
+    "^": 3,
+    "/": 2,
+    "*": 2,
+    "+": 1,
+    "-": 1,
+  };
+
+  // While there is input left, keep going
+  while (inputQueue.peek()) {
+    const inputSymbol = inputQueue.dequeue();
+
+    // IS AN OPERATOR
+    if (isNaN(Number(inputSymbol))) {
+      while (
+        // If the operator stack isn't empty
+        operatorStack.peek() &&
+        // The top operator ISN'T start parenthesis
+        operatorStack.peek().data != "(" &&
+        // AND the top operator has higher OR equal precedence to the new one
+        precedence[operatorStack.peek().data] >= precedence[inputSymbol] &&
+        // AND the new operator is "left-associative" (it reads from left to right)
+        // Exponentiation is the only right-associative operator (reading from right to left)
+        inputSymbol != "^"
+      ) {
+        // Remove the top operator, access it's data and put it in the output queue
+        const topOperator = operatorStack.pop().data;
+        outputQueue.enqueue(topOperator);
+      }
+      // Finally, push the new operator to the operator stack
+
+      if (inputSymbol == ")") {
+        while (operatorStack.peek() && operatorStack.peek().data != "(") {
+          const topOperator = operatorStack.pop().data;
+          outputQueue.enqueue(topOperator);
+        }
+        // The top operator is a left parenthesis since we exited the while loop!
+        // ... or the list is empty and it's an invalid argument, but it still shouldn't crash the program
+        operatorStack.pop();
+      } else {
+        operatorStack.push(inputSymbol);
+      }
+    }
+    // IS A NUMBER
+    else {
+      outputQueue.enqueue(inputSymbol);
+    }
+  }
+
+  while (operatorStack.peek()) {
+    const leftOverOperator = operatorStack.pop();
+    if (leftOverOperator.data != "(") {
+      outputQueue.enqueue(leftOverOperator.data);
+    }
+  }
+
+  console.log("Postfix string:");
+  outputQueue.dumpList();
+}
+
+// convertInfixToPostfix("( 1 + 2 ) ^ 2");
+// convertInfixToPostfix("1 + 2 * 4 - 3");
+convertInfixToPostfix("5 - 2 * 3 + 3 ^ 4");
+// convertInfixToPostfix("( 1 + 2 )");
