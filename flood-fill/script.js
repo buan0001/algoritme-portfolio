285608c0e62ea5937caaf97247bf8e2be4e9909a
buan0001 <bustermikkel@gmail.com>
2024-10-28 17:05:56 +0100

    Flood fill

--- flood-fill/script.js
+++ flood-fill/script.js
@@ -1,4 +1,8 @@
 "use strict";
+
+import Queue from "./Queue.js";
+import Stack from "./Stack.js";
+
 window.addEventListener("load", start);
 
 let GRID_WIDTH;
@@ -9,6 +13,32 @@ function start() {
   GRID_WIDTH = gridArray[0].length;
   buildGrid(GRID_WIDTH, GRID_HEIGHT);
   requestAnimationFrame(loop);
+  addEventListeners();
+}
+
+function addEventListeners() {
+  document.querySelector("#stack-btn").addEventListener("click", () => floodFillStack(0, 0));
+  document.querySelector("#recursive-btn").addEventListener("click", () => floodFill(0, 0));
+  document.querySelector("#queue-btn").addEventListener("click", () => floodFillQueue(0, 0));
+  document.querySelector("#reset-btn").addEventListener("click", resetGame);
+}
+
+function resetGame() {
+  console.log("resetting game");
+  for (let i = 0; i < gridArray.length; i++) {
+    for (let j = 0; j < gridArray[0].length; j++) {
+      if (gridArray[i][j] != 1) {
+        gridArray[i][j] = 0;
+      }
+    }
+  }
+  if (stackTimer) {
+    clearTimeout(stackTimer);
+  }
+  if (queueTimer) {
+    clearTimeout(queueTimer);
+  }
+  console.log(gridArray);
 }
 
 function loop() {
@@ -95,6 +125,7 @@ function updateDisplay() {
           cell.classList.add("image");
           break;
         case 2:
+          cell.classList.remove("draw");
           cell.classList.add("set");
           break;
         case 3:
@@ -120,16 +151,122 @@ function clear(x, y) {
   }
 }
 
+function draw(x, y) {
+  if (y < gridArray.length && x < gridArray[y].length) {
+    gridArray[y][x] = 3;
+  }
+}
+
 /*
   The weird "Inside" function as defined by the Flood-fill algorithm
   really just returns true if the pixel isn't already colored in ...
  */
 function inside(x, y) {
-  if (y < gridArray.length && x < gridArray[y].length) {
+  if (x >= 0 && y >= 0 && y < gridArray.length && x < gridArray[y].length) {
     return gridArray[y][x] == 0;
   }
 
   return false;
 }
 
-/* TODO: Create floodfill function */
+// With queue
+const que = new Queue();
+let queueTimer;
+function floodFillQueue(x, y) {
+  que.enqueue({ x, y });
+
+  function processStack(x, y) {
+    if (x != undefined && y != undefined) {
+      set(x, y);
+    }
+    if (que.peek()) {
+      const topNode = que.dequeue();
+      const x = topNode.data.x;
+      const y = topNode.data.y;
+      if (inside(x, y)) {
+        draw(x, y);
+        // for (let i = -1; i <= 1; i++) {
+        //   for (let j = -1; j <= 1; j++) {
+        //     if (j !== 0 || i !== 0) {
+        //       que.enqueue({ x: x + i, y: y + j });
+        //     }
+        //   }
+        // }
+        que.enqueue({ x: x - 1, y });
+        que.enqueue({ x: x + 1, y });
+        que.enqueue({ x, y: y - 1 });
+        que.enqueue({ x, y: y + 1 });
+        // que.enqueue({ x: x + 1, y: y + 1 });
+        // que.enqueue({ x: x - 1, y: y - 1 });
+        // que.enqueue({ x: x - 1, y: y + 1 });
+        // que.enqueue({ x: x + 1, y: y - 1 });
+        queueTimer = setTimeout(() => processStack(x, y), 10);
+      } else {
+        processStack();
+      }
+    } else {
+      console.log("Done");
+    }
+  }
+  processStack();
+}
+
+// With stack
+const stack = new Stack();
+let stackTimer;
+function floodFillStack(x, y) {
+  stack.push({ x, y });
+  function processStack(x, y) {
+    if (x != undefined && y != undefined) {
+      set(x, y);
+    }
+    if (stack.peek()) {
+      const topNode = stack.pop();
+      const x = topNode.data.x;
+      const y = topNode.data.y;
+      if (inside(x, y)) {
+        draw(x, y);
+        for (let i = -1; i <= 1; i++) {
+          for (let j = -1; j <= 1; j++) {
+            if (j !== 0 || i !== 0) {
+              stack.push({ x: x + i, y: y + j });
+            }
+          }
+        }
+        // stack.push({ x: x - 1, y: y - 1 })
+        // stack.push({ x: x + 1, y: y + 1 })
+        // stack.push({ x: x + 1, y: y - 1 })
+        // stack.push({ x: x - 1, y: y + 1 })
+        // stack.push({ x: x - 1, y });
+        // stack.push({ x: x + 1, y });
+        // stack.push({ x, y: y - 1 });
+        // stack.push({ x, y: y + 1 });
+        stackTimer = setTimeout(() => processStack(x, y), 10);
+      } else {
+        processStack();
+      }
+    } else {
+      console.log("Done");
+    }
+  }
+
+  processStack();
+}
+// Classic recursive solution
+function floodFill(x, y) {
+  if (!inside(x, y)) {
+    console.log("not inside");
+
+    return;
+  }
+
+  set(x, y);
+  setTimeout(() => {
+    floodFill(x - 1, y);
+    floodFill(x + 1, y);
+    floodFill(x, y - 1);
+    floodFill(x, y + 1);
+  }, 50);
+
+  return;
+}
